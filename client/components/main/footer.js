789e60793692094bf3c983274b50b1ce5d38fa34
Kacxxia <kacxxia@gmail.com>
2017-10-12 16:29:20 +0800

    添加favicon 首屏等待动画

--- client/components/main/footer.js
+++ client/components/main/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <foot  className='h-100 px-1 d-flex align-items-center' >
-            Copyright 2017
+            @Kacxxia 2017
         </foot>
     );
 };
