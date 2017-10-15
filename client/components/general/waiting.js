0243ffa0d76e8ab918d50ed175e320198964d7f6
Kacxxia <kacxxia@gmail.com>
2017-10-15 18:52:26 +0800

    修复BUG 优化体验 精简部分代码

--- client/components/general/waiting.js
+++ client/components/general/waiting.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CircularProgress from 'material-ui/CircularProgress'
-class Waiting extends Component {
-    render() {
-        return (
-            <div className="h-100 d-flex align-items-center justify-content-center ">
-               <CircularProgress size={80} thickness={12} />
-            </div>
-        );
-    }
+
+const Waiting = () => {
+    return (
+        <div className="h-100 d-flex align-items-center justify-content-center ">
+           <CircularProgress size={80} thickness={12} />
+        </div>
+    );
 }
 
 export default Waiting;
